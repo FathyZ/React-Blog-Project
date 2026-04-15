d2d43df0b796c69cfb9125b2463fca5da85a0efb
FathyZ <hammouda.fathy@gmail.com>
2026-04-15 04:10:40 +0200

    fixing auth endpoints #2

--- DATABASE/server.js
+++ DATABASE/server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth'); 
 const cors = require('cors'); 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const auth = require('json-server-auth'); 
 
+
+server.db = router.db;
 server.use(cors()); 
 
 server.use(middlewares);
